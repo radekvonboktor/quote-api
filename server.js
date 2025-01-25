1833ee64f65020c7abf73dc8bfa10f683e4dee8c
Mars Avdjiev <radek.von.boktor@gmail.com>
2025-01-25 17:47:02 +0200

    add get/post functionality

--- server.js
+++ server.js
@@ -11,3 +11,27 @@ app.use(express.static('./public'));
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`)
 })
+
+app.get('/api/quotes/random', (req, res, next) => {
+    res.send(getRandomElement(quotes));
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    if (req.query.person) {
+        const quoteToReturn = quotes.filter((quote) => quote.person === req.query.person);
+        res.send(quoteToReturn);
+    } else {
+        res.send(quotes);
+    }
+});
+
+app.post('/api/quotes', (req, res, next) => {
+    const objToCreate = req.query;
+
+    if (objToCreate) {
+        quotes.push(objToCreate);
+        res.send(objToCreate);
+    } else {
+        res.status(400);
+    }
+});
