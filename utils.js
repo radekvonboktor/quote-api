2ce724a9515c39bc1d790407e1d88ac80085fa6f
Mars Avdjiev <radek.von.boktor@gmail.com>
2025-01-26 09:51:07 +0200

    Adding new functionality

--- utils.js
+++ utils.js
@@ -3,6 +3,25 @@ const getRandomElement = arr => {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+const updateElement = (id, queryArguments, elementList) => {
+  const elementIndex = getIndexById(id, elementList);
+  if (elementIndex === -1) {
+    throw new Error('updateElement must be called with a valid id parameter');
+  }
+  if (queryArguments.id) {
+    queryArguments.id = Number(queryArguments.id);
+  }
+  Object.assign(elementList[elementIndex], queryArguments);
+  return elementList[elementIndex];
+};
+
+const getIndexById = (id, elementList) => {
+  return elementList.findIndex((element) => {
+    return element.id === Number(id);
+  });
+};
+
 module.exports = {
-  getRandomElement
+  getRandomElement,
+  updateElement
 };
